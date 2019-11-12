a25f017bbb4e772ca554d8d6a316b99c7ef4343b
Don Townsend <don@teamairship.com>
2019-11-12 10:11:54 -0500

    finalize db parse script

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ function main() {
   if (!process.argv || !process.argv.length > 1) {
     errorDie('You need to supply the database url as the first command line arg.')
   }
-  const dbUrl = process.argv[1];
 
+  const dbUrl = process.argv[2];
   const dbConfig = parseDbUrl(dbUrl);
 
-  console.log(dbConfig);
+  printLine('DB User', dbConfig.user);
+  printLine('DB Pass', dbConfig.password);
+  printLine('DB Host', dbConfig.host);
+  printLine('DB Port', dbConfig.port);
+  printLine('DB used', dbConfig.database);
 
-  // parse the db and display
   exitSuccess();
+}
+
+function printLine(label, val) {
+  process.stdout.write(`${label || ''}: `);
+  process.stdout.write(val || '');
+  process.stdout.write('\n');
 }
 
 function errorDie(err) {
@@ -23,7 +32,6 @@ function errorDie(err) {
 
 function exitSuccess(msg) {
   if (msg) { console.log(msg); }
-  console.log('success!!');
   process.exit(0);
 }
 
